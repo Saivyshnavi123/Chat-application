e73b0b7bc2a4e981a1d24299be061e3e7a8cc68b
Koushik168 <koushikrayavaram@gmail.com>
2025-07-10 00:32:57 +0530

    Create app.py
    python code

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template, session, redirect, url_for, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Random import get_random_bytes
+import base64
+import logging
+import hashlib
+import requests
